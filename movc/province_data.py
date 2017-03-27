5573f61dd1ec06431ef19ca87816b4cefe656b92
Piergiorgio Palla <piepalla@ASSTURISMO2>
2017-03-27 13:14:13 +0200

    a module containing some useful constants related to the old and new provinces

--- movc/province_data.py
+++ movc/province_data.py
@@ -2,7 +2,8 @@ __author__ = 'pg'
 
 
 # package costants
-NEW_PROVINCES = ['città metropolitana di cagliari', 'sud sardegna', 'sassari', 'oristano', 'nuoro']
 OLD_PROVINCE_NUMBER = 8
+NEW_PROVINCES = ['città metropolitana di cagliari', 'sud sardegna', 'sassari', 'oristano', 'nuoro']
 NEW_CODES = {"cagliari": '292', "sassari": '90', "sud sardegna": '111', "oristano": '95', "nuoro": '91'}
+
 PROVINCIAL_SYMOBOLS = {"cagliari": "ca", "sud sardegna": "sd", "sassari": "ss", "oristano": "or", "nuoro": "nu"}
