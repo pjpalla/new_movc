63f52b51f7860a2886b21bbb81279f82b0c611a4
pg <palla.pg@gmail.com>
2017-04-21 15:07:05 +0200

    * add methods get_alloggi, get_campeggi, get_altri alloggi to tha class All7 * refactor method get_alberghi * add constants to all7_consts module

--- all7/all7.py
+++ all7/all7.py
@@ -17,47 +17,59 @@ class All7:
         self.movc = load_workbook(movc_xl_path)
         self.sheets = self.movc.get_sheet_names()
 
-
-    def get_arrivi_residenti_alberghi(self):
-        tot_arrivi = 0
+    def get_alberghi(self, type='arrivi', category='residenti'):
+        tot = 0
         for sheet_name in self.sheets:
             current_sheet = self.movc.get_sheet_by_name(sheet_name)
-            print(current_sheet.title)
-            for i in RESIDENTS_RANGE:
-                idx = 'O' + str(i)
-                print(current_sheet[idx].value)
-                tot_arrivi += int(current_sheet[idx].value)
+            # print(current_sheet.title)
+            RANGE = RESIDENTS_RANGE if category == 'residenti' else NON_RESIDENTS_RANGE
+            for i in RANGE:
+                idx = 'O' + str(i) if type == 'arrivi' else 'P' + str(i)
+                # print(current_sheet[idx].value)
+                tot += int(current_sheet[idx].value)
+        print(tot)
+        return(tot)
 
-        print(tot_arrivi)
-        return(tot_arrivi)
-
-
-    def get_presenze_residenti_alberghi(self):
-        tot_presenze = 0
-        for sheet_name in self.sheets:
-            current_sheet = self.movc.get_sheet_by_name(sheet_name)
-            print(current_sheet.title)
-            for i in RESIDENTS_RANGE:
-                idx = 'P' + str(i)
-                print(current_sheet[idx].value)
-                tot_presenze += int(current_sheet[idx].value)
-        print(tot_presenze)
-        return(tot_presenze)
-
-    def get_arrivi_alloggi(self):
-        tot_arrivi = 0
+    def get_alloggi(self, type='arrivi', category='residenti'):
+        tot = 0
         #sheet_name = self.sheets[0]
-        for sheet_name in self.sheets[0:2]:
+        for sheet_name in self.sheets:
             current_sheet = self.movc.get_sheet_by_name(sheet_name)
-            for row_idx in RESIDENTS_RANGE:
+            # print(current_sheet.title)
+            RANGE = RESIDENTS_RANGE if category == 'residenti' else NON_RESIDENTS_RANGE
+            for row_idx in RANGE:
                 for k in ALLOGGI_DICT.keys():
-                    idx = ALLOGGI_DICT[k] + str(row_idx)
-                    #print(idx)
-                    #print(current_sheet[idx].value)
-                    tot_arrivi += current_sheet[idx].value
+                    value_idx = 0 if (type == 'arrivi') else 1
+                    idx = ALLOGGI_DICT[k][value_idx] + str(row_idx)
+                    tot += current_sheet[idx].value
                     #print("row id: " + str(row_idx) + " " + str(tot_arrivi))
-        return(tot_arrivi)
-        
+        return(tot)
+
+    def get_campeggi(self, type="arrivi", category='residenti'):
+        tot = 0
+        for sheet_name in self.sheets:
+            current_sheet = self.movc.get_sheet_by_name(sheet_name)
+            RANGE = RESIDENTS_RANGE if category == 'residenti' else NON_RESIDENTS_RANGE
+            for row_idx in RANGE:
+                for k in CAMPEGGI_DICT.keys():
+                    value_idx = 0 if (type == "arrivi") else 1
+                    idx = CAMPEGGI_DICT[k][value_idx] + str(row_idx)
+                    # print(idx)
+                    tot += current_sheet[idx].value
+        return(tot)
+
+    def get_altri_alloggi(self, type='arrivi', category='residenti'):
+        tot = 0
+        for sheet_name in self.sheets:
+            current_sheet = self.movc.get_sheet_by_name(sheet_name)
+            RANGE = RESIDENTS_RANGE if category == 'residenti' else NON_RESIDENTS_RANGE
+            for row_idx in RANGE:
+                for k in ALTRI_ALLOGGI_DICT.keys():
+                    value_idx = 0 if (type == "arrivi") else 1
+                    idx = ALTRI_ALLOGGI_DICT[k][value_idx] + str(row_idx)
+                    # print(idx)
+                    tot += current_sheet[idx].value
+        return(tot)
 
 
 
--- all7/all7_consts.py
+++ all7/all7_consts.py
@@ -1,7 +1,10 @@
 __author__ = 'pg'
 
 ### range of cells of the movc related to people resident in Italy
-RESIDENTS_RANGE = list(range(16,37))
+RESIDENTS_RANGE = list(range(16, 37))
+NON_RESIDENTS_RANGE = list(range(39, 96))
 
 ###
-ALLOGGI_DICT = {"B2":"S", "B4":"W", "B5":"Y", "B6":"AA", "B7":"AC", "B8":"AE", "B9": "AG", "C1":"AK"}
+ALLOGGI_DICT = {"B2":["S","T"], "B4":["W", "X"], "B5":["Y", "Z"], "B6":["AA", "AB"], "B7":["AC", "AD"], "B8":["AE", "AF"], "B9": ["AG", "AH"], "C1":["AK", "AL"]}
+CAMPEGGI_DICT = {"B1": ["Q", "R"], "B3":["U", "V"]}
+ALTRI_ALLOGGI_DICT = {"C2":["AM", "AN"]}
