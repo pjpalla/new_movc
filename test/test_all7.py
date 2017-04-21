e085dddeca9569c4f3acb5b2fc130a11d64a9005
pg <palla.pg@gmail.com>
2017-04-21 15:08:09 +0200

    * add new unit tests for the methods of the All7 class

--- test/test_all7.py
+++ test/test_all7.py
@@ -15,9 +15,9 @@ class TestAll7(unittest.TestCase):
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
 
-        self.template_path = r"config/allegato7_base.xlsx"
+        self.template_path = r"C:\Users\piepalla\PycharmProjects\new_movc\config\allegato7_base.xlsx"
         # self.province_dir = r"C:\Users\piepalla\PycharmProjects\new_movc\all7\movc\cagliari\MOVC_CA_GENNAIO_2016.xlsx"
-        self.file_example = r"MOVC_CA_GENNAIO_2016.xlsx"
+        self.file_example = r"C:\Users\piepalla\PycharmProjects\new_movc\all7\movc\cagliari\MOVC_CA_GENNAIO_2016.xlsx"
         self.all7 = All7(2016, "città metropolitana di cagliari", self.template_path)
         self.all7.load_xl(self.file_example)
 
@@ -27,15 +27,53 @@ class TestAll7(unittest.TestCase):
         pass
 
 
-    def test_get_arrivi_residenti_alberghi(self):
-        tot_arrivi = self.all7.get_arrivi_residenti_alberghi()
-        self.logger.debug(tot_arrivi)
+    def test_get_alberghi(self):
+        tot_arrivi_residenti = self.all7.get_alberghi()
+        self.logger.debug(tot_arrivi_residenti)
+
+        tot_arrivi_non_residenti = self.all7.get_alberghi(category='non residenti')
+        self.logger.debug(tot_arrivi_non_residenti)
+
+        tot_presenze_residenti = self.all7.get_alberghi(type='presenze')
+        self.logger.debug(tot_presenze_residenti)
+        tot_presenze_non_residenti = self.all7.get_alberghi(type='presenze', category='non residenti')
+        self.logger.debug(tot_presenze_non_residenti)
 
 
-    def test_get_presenze_residenti_alberghi(self):
-        tot_presenze = self.all7.get_presenze_residenti_alberghi()
-        self.logger.debug(tot_presenze)
+    # def test_get_presenze_residenti_alberghi(self):
+    #     tot_presenze = self.all7.get_presenze_residenti_alberghi()
+    #     self.logger.debug(tot_presenze)
 
-    def test_get_arrivi_alloggi(self):
-        tot_arrivi = self.all7.get_arrivi_alloggi()
-        self.assertEqual(tot_arrivi, 1492)
+    def test_get_alloggi(self):
+        tot_arrivi_residenti = self.all7.get_alloggi()
+        self.assertEqual(tot_arrivi_residenti, 1716)
+        tot_arrivi_non_residenti = self.all7.get_alloggi(category='non residenti')
+
+        tot_presenze_residenti = self.all7.get_alloggi(type='presenze')
+        self.logger.debug(tot_presenze_residenti)
+        tot_presenze_non_residenti = self.all7.get_alloggi(type='presenze', category='non residenti')
+        self.logger.debug(tot_presenze_non_residenti)
+
+
+    def test_get_campeggi(self):
+        tot_arrivi_residenti = self.all7.get_campeggi()
+        self.logger.debug(tot_arrivi_residenti)
+        tot_arrivi_non_residenti = self.all7.get_campeggi(category='non residenti')
+        self.logger.debug(tot_arrivi_non_residenti)
+
+        tot_presenze_residenti = self.all7.get_campeggi(type='presenze')
+        self.logger.debug(tot_presenze_residenti)
+        tot_presenze_non_residenti = self.all7.get_campeggi(type='presenze', category='non residenti')
+        self.logger.debug(tot_presenze_non_residenti)
+
+
+    def test_get_altri_alloggi(self):
+        tot_arrivi_residenti = self.all7.get_altri_alloggi()
+        self.logger.debug(tot_arrivi_residenti)
+        tot_arrivi_non_residenti = self.all7.get_altri_alloggi(category='non residenti')
+        self.logger.debug(tot_arrivi_non_residenti)
+
+        tot_presenze_residenti = self.all7.get_altri_alloggi(type="presenze")
+        self.logger.debug(tot_presenze_residenti)
+        tot_presenze_non_residenti = self.all7.get_altri_alloggi(type='presenze', category='non residenti')
+
