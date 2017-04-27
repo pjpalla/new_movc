81fe5410f50454c6e377e3af80cd094500356d01
pg <palla.pg@gmail.com>
2017-04-27 14:25:56 +0200

    add constants

--- all7/all7_consts.py
+++ all7/all7_consts.py
@@ -8,3 +8,7 @@ NON_RESIDENTS_RANGE = list(range(39, 96))
 ALLOGGI_DICT = {"B2":["S","T"], "B4":["W", "X"], "B5":["Y", "Z"], "B6":["AA", "AB"], "B7":["AC", "AD"], "B8":["AE", "AF"], "B9": ["AG", "AH"], "C1":["AK", "AL"]}
 CAMPEGGI_DICT = {"B1": ["Q", "R"], "B3":["U", "V"]}
 ALTRI_ALLOGGI_DICT = {"C2":["AM", "AN"]}
+
+
+###MOVC SOURCE DIR###
+SOURCE_DIR = r"C:\Users\piepalla\PycharmProjects\new_movc\all7\movc"
