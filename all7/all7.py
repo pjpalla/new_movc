09ea85a7461ad5bb3854f657edbdd37688a9ac12
pg <palla.pg@gmail.com>
2017-04-27 14:24:52 +0200

    add methods to generate all7 module

--- all7/all7.py
+++ all7/all7.py
@@ -5,7 +5,7 @@ from movc.province_data import *
 from all7.all7_consts import *
 
 class All7:
-    def __init__(self, year, province, template_file_path):
+    def __init__(self, template_file_path, year, province):
         self.year = year
         self.template = load_workbook(template_file_path)
         if province in NEW_PROVINCES:
@@ -14,8 +14,11 @@ class All7:
             raise Exception("Illegal province")
 
     def load_xl(self, movc_xl_path):
-        self.movc = load_workbook(movc_xl_path)
+        self.movc = load_workbook(movc_xl_path, data_only=True)
         self.sheets = self.movc.get_sheet_names()
+        if self.sheets[-1] == 'Riepilogo':
+            self.sheets = self.sheets[0:-1]
+
 
     def get_alberghi(self, type='arrivi', category='residenti'):
         tot = 0
@@ -70,6 +73,35 @@ class All7:
                     # print(idx)
                     tot += current_sheet[idx].value
         return(tot)
+
+
+    def get_giornate_letto(self):
+        tot = 0
+        for sheet_name in self.sheets:
+            # print(sheet_name)
+            current_sheet = self.movc.get_sheet_by_name(sheet_name)
+            idx = 'P11'
+            tot += int(current_sheet[idx].value)
+        return(tot)
+
+
+    def get_giornate_camere(self, type = 'disponibili'):
+        tot = 0
+        idx = 'P12' if (type == 'disponibili') else 'P13'
+        for sheet_name in self.sheets[0:3]:
+            # print(sheet_name)
+            current_sheet = self.movc.get_sheet_by_name(sheet_name)
+            tot += int(current_sheet[idx].value)
+        return(tot)
+
+
+    def build_xl(self, movc_dir):
+        file_names = []
+        try:
+            file_names = os.listdir(movc_dir)
+        except NotADirectoryError():
+            print("Wrong directory! Please enter the correct movc directory name")
+            return
 
 
 
