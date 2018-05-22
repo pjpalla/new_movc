6241b186f7fe70add19173360b6c7ed074d3e88e
pg <palla.pg@gmail.com>
2018-05-22 10:06:02 +0200

    * define some useful constants for the ctt4 class

---
+++ ctt4/ctt4_consts.py
@@ -0,0 +1,11 @@
+__author__ = 'pg'
+
+MUNICIPALITY_COL_IDX = 'B'
+MUNICIPALITY_ROW_LOWER_IDX = 9
+
+CTT4_SHEET1 = 'Sez 1'
+CTT4_SHEET2 = 'Sez 2'
+
+BASE_DIR = r""
+TEMPLATE_PATH = r"C:\Users\piepalla\PycharmProjects\new_movc\config\ctt4_base.xlsx"
+MAPPING_PATH = r"C:\Users\piepalla\PycharmProjects\new_movc\config\mapping.csv"
