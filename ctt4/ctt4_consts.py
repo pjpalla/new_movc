bd32995d7ba97087309095dbe08fdf6a4635bd47
pg <palla.pg@gmail.com>
2018-05-30 10:40:19 +0200

    * add new constants to manage total values

--- ctt4/ctt4_consts.py
+++ ctt4/ctt4_consts.py
@@ -6,6 +6,19 @@ MUNICIPALITY_ROW_LOWER_IDX = 9
 CTT4_SHEET1 = 'Sez 1'
 CTT4_SHEET2 = 'Sez 2'
 
+SHEET1_CELL_LIST = ["A4", "B4", "C4", "D6", "H6", "L6", "P6", "T6", "X6", "AF4", "AZ6"]
+SHEET2_CELL_LIST = ["A4", "B4", "C4", "D6", "H6", "L6", "P6","U4", "D4", "D5"]
+
+SHEET1_TOT_CELL = ["AB", "AC", "AD", "AE"]
+SHEET2_TOT_CELL = ["P", "Q", "R", "S"]
+
+
 BASE_DIR = r""
 TEMPLATE_PATH = r"C:\Users\piepalla\PycharmProjects\new_movc\config\ctt4_base.xlsx"
 MAPPING_PATH = r"C:\Users\piepalla\PycharmProjects\new_movc\config\mapping.csv"
+
+PROVINCE_CELL_COORDINATE = "Q2"
+PROVINCE_CODE_COORDINATE = "S2"
+SELECTED_YEAR_CELL = "B3"
+
+PACE = 4
